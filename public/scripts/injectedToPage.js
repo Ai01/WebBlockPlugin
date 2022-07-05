38e00b780aa3a3256324edd92b60c8daff47292e
bytedance <baihaihui@bytedance.com>
2022-07-05 17:33:43 +0800

    fix: redirect after short browser

--- public/scripts/injectedToPage.js
+++ public/scripts/injectedToPage.js
@@ -2,7 +2,7 @@
 // if background say this page should be replace,
 // the html will be overwrite
 
-const getTemplate = (data) =>`
+const getTemplate = (data) => `
   <html>
     <div style="position:absolute;top:0;bottom:0;left:0;right:0;display:flex;justify-content:center;align-items:center">
       <div style="font-size:26px;font-weight:bold;text-align:center;width:100%;word-break: break-word;white-space: pre-line;padding:0px 200px;">${data}</div>
@@ -38,10 +38,16 @@ chrome.runtime.sendMessage(
       return;
     }
 
-    if (belling) {
+    if (belling && overwrite) {
       setTimeout(() => {
         const nextHtml = getTemplate(data);
         renderTemplate(nextHtml);
+      }, (Number(minutes) || 5) * 60 * 1000);
+    }
+
+    if (belling && redirect) {
+      setTimeout(() => {
+        window.location.replace(redirect);
       }, (Number(minutes) || 5) * 60 * 1000);
     }
   }
